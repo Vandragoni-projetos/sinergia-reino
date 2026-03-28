43043f150eb8f592ab7392577322f2f96a8fe28f
Vandragoni-projetos <vandragoni@gmail.com>
2026-03-28 13:23:43 -0300

    Update obrigado.php

--- obrigado.php
+++ obrigado.php
@@ -103,7 +103,7 @@ if (!function_exists('send_delivery_email_consolidated')) {
                 error_log("Usando SMTP: " . $config['smtp_host'] . ":" . $config['smtp_port']);
             }
             
-            $mail->setFrom($fromEmail, $config['smtp_from_name'] ?? 'GatewayPro');
+            $mail->setFrom($fromEmail, $config['smtp_from_name'] ?? 'Hub SinergIA');
             $mail->addAddress($to_email, $customer_name);
             
             // Usa o assunto do banco ou um padrão
